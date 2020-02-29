106928121bee9c33658634dd4150662616004e2e
David Montesdeoca <backpackerhh@gmail.com>
2020-02-29 23:11:45 +0100

    Allow to fetch comments from the fake JSON server

--- src/__test__/components/App.test.js
+++ src/__test__/components/App.test.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, wait, cleanup } from "@testing-library/react";
 
+import fakeServer from "../../apis/fakeServer";
 import App from "../../components/App";
 
-test("renders greeting", () => {
-  const { getByText } = render(<App />);
-  const greeting = getByText(/Hi from App/i);
+const comment_1 = {
+  id: 1,
+  body: "I do love writing tests",
+  author: "The Notester"
+};
+const comment_2 = {
+  id: 2,
+  body: "Nothing is better than a good comment app",
+  author: "Comment Hater"
+};
 
-  expect(greeting).toBeInTheDocument();
+afterEach(cleanup);
+
+beforeEach(() => {
+  fakeServer.get = jest.fn(() => Promise.resolve({ data: [comment_1, comment_2] }));
+});
+
+describe("App", () => {
+  it("fetches comments and renders them to the page", async () => {
+    const { getByText } = render(<App />);
+
+    await wait(() => getByText(comment_1.body));
+
+    expect(getByText(comment_1.author)).toBeDefined();
+    expect(getByText(comment_2.body)).toBeDefined();
+    expect(getByText(comment_2.author)).toBeDefined();
+  });
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import fakeServer from "../apis/fakeServer";
+import CommentForm from "./CommentForm";
+import CommentList from "./CommentList";
 
 const App = () => {
-  return <div>Hi from App!</div>;
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fakeServer.get("/comments");
+
+      setComments(response.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <CommentForm />
+      <CommentList comments={comments} />
+    </div>
+  );
 };
 
 export default App;
