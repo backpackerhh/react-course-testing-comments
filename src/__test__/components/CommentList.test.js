7308a38643c01c1668da80763d255080e8bcd6b4
David Montesdeoca <backpackerhh@gmail.com>
2020-02-29 16:14:51 +0100

    Add CommentList components with tests

---
+++ src/__test__/components/CommentList.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import CommentList from "../../components/CommentList";
+
+describe("Comment List", () => {
+  it("renders a list of comment cards with their comment and author", () => {
+    // Arrange
+    const comment_1 = {
+      id: 1,
+      body: "I do love writing tests",
+      author: "The Notester"
+    };
+    const comment_2 = {
+      id: 2,
+      body: "Nothing is better than a good comment app",
+      author: "Comment Hater"
+    };
+
+    // Act
+    const { getByText } = render(<CommentList comments={[comment_1, comment_2]} />);
+
+    // Assert
+    expect(getByText(comment_1.body)).toBeDefined();
+    expect(getByText(comment_1.author)).toBeDefined();
+    expect(getByText(comment_2.body)).toBeDefined();
+    expect(getByText(comment_2.author)).toBeDefined();
+  });
+});
